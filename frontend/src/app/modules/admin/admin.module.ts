737b5c2fb0d257cd698e1e055679e77cf2833661
unknown <rsv.kpast@gmail.com>
2020-09-13 13:03:47 +0300

    admin and admin-side-menu components basic init

--- frontend/src/app/modules/admin/admin.module.ts
+++ frontend/src/app/modules/admin/admin.module.ts
@@ -2,9 +2,11 @@ import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './components/admin/admin.component';
+import { AdminSideMenuComponent } from './components/admin-side-menu/admin-side-menu.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent, AdminSideMenuComponent],
   imports: [AdminRoutingModule, SharedModule],
 })
 export class AdminModule {}
