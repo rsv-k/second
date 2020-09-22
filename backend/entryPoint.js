944ad65e807e07a1e7536c84bf94792e765a9e29
unknown <rsv.kpast@gmail.com>
2020-09-22 15:08:35 +0300

    serving static images added

--- backend/entryPoint.js
+++ backend/entryPoint.js
@@ -1,12 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const errorHandler = require("./middlewares/error");
-const currencyRoutes = require("./routes/currency");
+const errorHandler = require('./middlewares/error');
+const currencyRoutes = require('./routes/currency');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/currency", currencyRoutes);
+app.use('/images', express.static(path.join('images')));
+
+app.use('/api/currency', currencyRoutes);
 
 app.use(errorHandler);
 
