e3fce78961814453ac53c5de87f06d700f85950b
unknown <rsv.kpast@gmail.com>
2020-09-22 13:19:28 +0300

    ngrx add currency implemented

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
+const entryPoint = require("./entryPoint");
 
 if (!process.env.production) {
    require("dotenv").config();
 }
+
+app.use(entryPoint);
 
 app.listen(PORT, () => {
    console.log("server is running");
