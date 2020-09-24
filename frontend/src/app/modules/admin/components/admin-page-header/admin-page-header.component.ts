7ae3da0083c3c621179a0626fe7f07d87ffce8b6
unknown <rsv.kpast@gmail.com>
2020-09-24 16:13:38 +0300

    pages data moved to admin-page-header

--- frontend/src/app/modules/admin/components/admin-page-header/admin-page-header.component.ts
+++ frontend/src/app/modules/admin/components/admin-page-header/admin-page-header.component.ts
@@ -2,7 +2,8 @@ import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { Component, Input, OnInit } from '@angular/core';
 import * as fromApp from '../../../../store/index';
-import { map, pluck } from 'rxjs/operators';
+import { filter, map, pluck } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
    selector: 'app-admin-page-header',
@@ -14,14 +15,55 @@ export class AdminPageHeaderComponent implements OnInit {
    @Input() linkTo: string;
    currenciesAmount$: Observable<number>;
 
-   constructor(private store: Store<fromApp.AppState>) {}
+   private pagesData = {
+      'exchanges-show': {
+         title: 'Направления обмена',
+         linkTo: 'exchanges-create',
+      },
+      'currencies-show': {
+         title: 'Валюты',
+         linkTo: 'currencies-create',
+      },
+      'orders-show': {
+         title: 'Заявки на обмен',
+      },
+      'currencies-create': {
+         title: 'Добавить валюту',
+      },
+      'exchanges-create': {
+         title: 'Добавить направление обмена',
+      },
+      'currencies-edit': {
+         title: 'Редактировать валюту',
+      },
+   };
+   constructor(
+      private store: Store<fromApp.AppState>,
+      private router: Router
+   ) {}
 
    ngOnInit(): void {
+      this.updatePageData();
+
+      this.router.events
+         .pipe(filter((e) => e instanceof NavigationEnd))
+         .subscribe(() => {
+            this.updatePageData();
+         });
+
       this.currenciesAmount$ = this.store.select('admin').pipe(
          pluck('currenciesAmount'),
          map((num) => {
             return num > 0 && this.title === 'Валюты' ? num : null;
          })
       );
+   }
+
+   private updatePageData(): void {
+      const arr = this.router.url.split('/');
+      const page = arr.length === 4 ? arr[2] : arr.pop();
+
+      this.title = this.pagesData[page].title;
+      this.linkTo = this.pagesData[page].linkTo;
    }
 }
--- frontend/src/app/modules/admin/components/admin/admin.component.ts
+++ frontend/src/app/modules/admin/components/admin/admin.component.ts
@@ -1,53 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, Event } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
    selector: 'app-admin',
    templateUrl: './admin.component.html',
    styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent implements OnInit {
-   title: string;
-   linkTo: string;
-
-   private pagesData = {
-      'exchanges-show': {
-         title: 'Направления обмена',
-         linkTo: 'exchanges-create',
-      },
-      'currencies-show': {
-         title: 'Валюты',
-         linkTo: 'currencies-create',
-      },
-      'orders-show': {
-         title: 'Заявки на обмен',
-      },
-      'currencies-create': {
-         title: 'Добавить валюту',
-      },
-      'exchanges-create': {
-         title: 'Добавить направление обмена',
-      },
-      'currencies-edit': {
-         title: 'Редактировать валюту',
-      },
-   };
-
-   constructor(private router: Router) {}
-
-   ngOnInit(): void {
-      this.updatePageData();
-
-      this.router.events.subscribe((event: Event) => {
-         this.updatePageData();
-      });
-   }
-
-   private updatePageData(): void {
-      const arr = this.router.url.split('/');
-      const page = arr.length === 4 ? arr[2] : arr.pop();
-
-      this.title = this.pagesData[page].title;
-      this.linkTo = this.pagesData[page].linkTo;
-   }
-}
+export class AdminComponent {}
