c868a123bf42e046a937ba1e1462bb0b759fd5da
unknown <rsv.kpast@gmail.com>
2020-09-27 10:58:40 +0300

    ngrx exchange update added

--- controllers/exchange.js
+++ controllers/exchange.js
@@ -103,3 +103,37 @@ exports.getExchange = async (req, res, next) => {
       next(error);
    }
 };
+
+exports.editExchange = async (req, res, next) => {
+   try {
+      const id = req.params.id;
+      const body = req.body;
+      if (!mongooseHelper.isValidId(id)) {
+         const error = new Error('Exchange not found');
+         error.statusCode = 404;
+         return next(error);
+      }
+
+      if (Object.keys(body).length === 0) {
+         const error = new Error('Incomplete data');
+         error.statusCode = 422;
+         return next(error);
+      }
+
+      let exchange = await Exchange.findById(id);
+      if (!exchange) {
+         const error = new Error('Exchange not found');
+         error.statusCode = 404;
+         return next(error);
+      }
+
+      exchange = await Exchange.findByIdAndUpdate(id, body, { new: true })
+         .populate('takenCurrency')
+         .populate('givenCurrency');
+
+      res.status(200).json({ msg: 'Exchange updated successfully', exchange });
+   } catch (err) {
+      const error = new Error('Internal server error');
+      next(error);
+   }
+};
