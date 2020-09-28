cc185eb4156f8e2a5df9dd302c0803d7a66f8994
unknown <rsv.kpast@gmail.com>
2020-09-28 11:30:39 +0300

    header basic implemented

--- frontend/src/app/modules/main/main.module.ts
+++ frontend/src/app/modules/main/main.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from './../../shared/shared.module';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-   declarations: [MainComponent],
+   declarations: [MainComponent, HeaderComponent],
    imports: [MainRoutingModule, SharedModule],
 })
 export class MainModule {}
