0c4b980b2a5f35c0a4e9f13ab7901f4279bf3895
unknown <rsv.kpast@gmail.com>
2020-09-28 11:38:42 +0300

    footer basic implemented

--- frontend/src/app/modules/main/main.module.ts
+++ frontend/src/app/modules/main/main.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from './../../shared/shared.module';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-   declarations: [MainComponent, HeaderComponent],
+   declarations: [MainComponent, HeaderComponent, FooterComponent],
    imports: [MainRoutingModule, SharedModule],
 })
 export class MainModule {}
