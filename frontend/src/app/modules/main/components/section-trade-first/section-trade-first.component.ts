1e947d449c0360e49de15f1a464ae8d0fb5e3e77
unknown <rsv.kpast@gmail.com>
2020-10-01 14:35:29 +0300

    section-trade-first currencies displayed

--- frontend/src/app/modules/main/components/section-trade-first/section-trade-first.component.ts
+++ frontend/src/app/modules/main/components/section-trade-first/section-trade-first.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../../../store/index';
+import * as ExchangeActions from '../../../../store/actions/exchange.actions';
+import { Observable } from 'rxjs';
+import { Exchange } from './../../../../core/models/exchange.model';
+import { pluck } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-section-trade-first',
-  templateUrl: './section-trade-first.component.html',
-  styleUrls: ['./section-trade-first.component.scss']
+   selector: 'app-section-trade-first',
+   templateUrl: './section-trade-first.component.html',
+   styleUrls: ['./section-trade-first.component.scss'],
 })
 export class SectionTradeFirstComponent implements OnInit {
+   exchanges$: Observable<Exchange[]>;
+   constructor(private store: Store<fromApp.AppState>) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+   ngOnInit(): void {
+      this.store.dispatch(ExchangeActions.loadExchangesStart());
+      this.exchanges$ = this.store.select('exchange').pipe(pluck('exchanges'));
+   }
 }
