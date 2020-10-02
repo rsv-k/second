f28d6c962d157f4122bc7c6f9d15649fe64a17b6
unknown <rsv.kpast@gmail.com>
2020-10-02 12:15:55 +0300

    section-trade-first display currencies

--- frontend/src/app/modules/main/components/section-trade-first/section-trade-first.component.ts
+++ frontend/src/app/modules/main/components/section-trade-first/section-trade-first.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../../../../store/index';
 import * as ExchangeActions from '../../../../store/actions/exchange.actions';
-import { Observable } from 'rxjs';
 import { Exchange } from './../../../../core/models/exchange.model';
 import { pluck } from 'rxjs/operators';
 
@@ -12,11 +11,49 @@ import { pluck } from 'rxjs/operators';
    styleUrls: ['./section-trade-first.component.scss'],
 })
 export class SectionTradeFirstComponent implements OnInit {
-   exchanges$: Observable<Exchange[]>;
+   givenCurrencies: Exchange[] = [];
+   takenCurrencies: Exchange[] = [];
+   currentExchange: Exchange;
+
+   private exchanges: Exchange[];
    constructor(private store: Store<fromApp.AppState>) {}
 
    ngOnInit(): void {
       this.store.dispatch(ExchangeActions.loadExchangesStart());
-      this.exchanges$ = this.store.select('exchange').pipe(pluck('exchanges'));
+      this.store
+         .select('exchange')
+         .pipe(pluck('exchanges'))
+         .subscribe((data) => {
+            this.exchanges = data;
+            this.getUniqueExchanges(data);
+            this.selectCurrentExchange(data[0]);
+         });
+   }
+
+   selectCurrentExchange(exchange: Exchange): void {
+      this.currentExchange = exchange;
+      this.getTakenCurrencies(exchange);
+   }
+
+   private getUniqueExchanges(exchanges: Exchange[]): void {
+      const checkedExchanges = {};
+
+      for (const e of exchanges) {
+         if (!checkedExchanges[e.givenCurrency.name]) {
+            this.givenCurrencies.push(e);
+            checkedExchanges[e.givenCurrency.name] = true;
+         }
+      }
+   }
+
+   private getTakenCurrencies(exchange: Exchange): void {
+      this.takenCurrencies = [];
+      const exchanges = this.exchanges;
+
+      for (const key of exchanges) {
+         if (key.givenCurrency.name === exchange.givenCurrency.name) {
+            this.takenCurrencies.push(key);
+         }
+      }
    }
 }
