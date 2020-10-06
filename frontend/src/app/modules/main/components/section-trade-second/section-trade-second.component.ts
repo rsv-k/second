44960088552bc62f0641cd953b0088864cb0726a
unknown <rsv.kpast@gmail.com>
2020-10-06 16:08:10 +0300

    section-trade-second form validation added

--- frontend/src/app/modules/main/components/section-trade-second/section-trade-second.component.ts
+++ frontend/src/app/modules/main/components/section-trade-second/section-trade-second.component.ts
@@ -1,6 +1,6 @@
 import { Exchange } from './../../../../core/models/exchange.model';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { first, pluck } from 'rxjs/operators';
@@ -15,6 +15,17 @@ import * as ProgressActions from '../../../../store/actions/progress.actions';
 export class SectionTradeSecondComponent implements OnInit {
    form: FormGroup;
    exchange: Exchange;
+
+   givenCurrencyName: string;
+   takenCurrencyName: string;
+
+   private currencyNames = {
+      EUR: 'евро',
+      UAH: 'гривнах',
+      USD: 'долларах',
+      RUB: 'рублях',
+   };
+
    constructor(
       private store: Store<fromApp.AppState>,
       private route: ActivatedRoute,
@@ -23,14 +34,6 @@ export class SectionTradeSecondComponent implements OnInit {
 
    ngOnInit(): void {
       this.store.dispatch(ProgressActions.setCurrentProcess({ payload: 2 }));
-      this.form = new FormGroup({
-         givenCurrency: new FormControl(''),
-         takenCurrency: new FormControl(''),
-         givenCard: new FormControl(''),
-         takenCard: new FormControl(''),
-         phone: new FormControl(''),
-         email: new FormControl(''),
-      });
 
       this.store
          .select('exchange')
@@ -44,8 +47,62 @@ export class SectionTradeSecondComponent implements OnInit {
             }
 
             this.exchange = exchange;
+            this.givenCurrencyName = this.currencyNames[
+               this.exchange.givenCurrency.currencyCode
+            ];
+            this.takenCurrencyName = this.currencyNames[
+               this.exchange.takenCurrency.currencyCode
+            ];
+
+            this.initForm();
          });
    }
 
-   onSubmit() {}
+   onGivenCurrencyChange(value: number): void {
+      if (!value || value < 0) {
+         return this.form.get('takenCurrency').patchValue('');
+      }
+
+      const calculatedCurrency = this.exchange.takenCurrencyCourse * value;
+      this.form.get('takenCurrency').patchValue(calculatedCurrency);
+   }
+
+   onTakenCurrencyChange(value: number): void {
+      if (!value || value < 0) {
+         return this.form.get('givenCurrency').patchValue('');
+      }
+
+      const calculatedCurrency = value / this.exchange.takenCurrencyCourse;
+      this.form.get('givenCurrency').patchValue(calculatedCurrency);
+   }
+
+   onSubmit(): void {
+      if (!this.form.valid) {
+         return;
+      }
+
+      console.log('valid');
+   }
+
+   private initForm(): void {
+      this.form = new FormGroup({
+         givenCurrency: new FormControl('', [
+            Validators.required,
+            Validators.min(this.exchange.minGivenCurrency),
+            Validators.max(this.exchange.maxGivenCurrency),
+         ]),
+         takenCurrency: new FormControl('', [
+            Validators.required,
+            Validators.min(
+               this.exchange.minGivenCurrency *
+                  this.exchange.takenCurrencyCourse
+            ),
+            Validators.max(this.exchange.takenCurrency.reserve),
+         ]),
+         givenCard: new FormControl('', [Validators.required]),
+         takenCard: new FormControl('', [Validators.required]),
+         phone: new FormControl('', [Validators.required]),
+         email: new FormControl('', [Validators.required, Validators.email]),
+      });
+   }
 }
