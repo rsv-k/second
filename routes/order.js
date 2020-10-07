40aa1adc5511d2f8370d66eddd729b9ec5c3db0b
unknown <rsv.kpast@gmail.com>
2020-10-07 12:21:43 +0300

    orders get all implemented

--- routes/order.js
+++ routes/order.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const orderController = require('../controllers/order');
 
 router.post('', orderController.createOrder);
+router.get('', orderController.getCurrencies);
 
 module.exports = router;
