0ba3af394536b83e299ad9c563bdf9619824c6f8
unknown <rsv.kpast@gmail.com>
2020-10-08 14:24:44 +0300

    display all exchanges in section-tariffs page

--- frontend/src/app/modules/main/components/section-tariffs/section-tariffs.component.ts
+++ frontend/src/app/modules/main/components/section-tariffs/section-tariffs.component.ts
@@ -3,6 +3,9 @@ import { Store } from '@ngrx/store';
 import { filter, first, pluck } from 'rxjs/operators';
 import * as fromApp from '../../../../store/index';
 import * as ExchangeActions from '../../../../store/actions/exchange.actions';
+import { Observable } from 'rxjs';
+import { Exchange } from 'src/app/core/models/exchange.model';
+import { Router } from '@angular/router';
 
 @Component({
    selector: 'app-section-tariffs',
@@ -10,20 +13,30 @@ import * as ExchangeActions from '../../../../store/actions/exchange.actions';
    styleUrls: ['./section-tariffs.component.scss'],
 })
 export class SectionTariffsComponent implements OnInit {
-   constructor(private store: Store<fromApp.AppState>) {}
+   exchanges$: Observable<Exchange[]>;
+   columnsToDisplay = ['givenCurrency', 'takenCurrency', 'reserve'];
+   constructor(
+      private store: Store<fromApp.AppState>,
+      private router: Router
+   ) {}
 
    ngOnInit(): void {
       this.store.dispatch(
          ExchangeActions.loadExchangesStart({ payload: { isSorted: true } })
       );
 
-      this.store
-         .select('exchange')
-         .pipe(
-            pluck('exchanges'),
-            filter((exchanges) => exchanges.length > 0),
-            first()
-         )
-         .subscribe((exchanges) => console.log(exchanges));
+      this.exchanges$ = this.store.select('exchange').pipe(
+         pluck('exchanges'),
+         filter((exchanges) => exchanges.length > 0),
+         first()
+      );
+   }
+
+   onNavigate(exchange: Exchange): void {
+      if (!exchange.isActive) {
+         return;
+      }
+
+      this.router.navigate(['/exchanges', exchange.id]);
    }
 }
