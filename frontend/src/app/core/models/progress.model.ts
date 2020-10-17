2351edd3ba2033e80a9354bc9438877a277cd86f
unknown <rsv.kpast@gmail.com>
2020-10-17 11:39:04 +0300

    section-trade-progress refactored

---
+++ frontend/src/app/core/models/progress.model.ts
@@ -0,0 +1,6 @@
+export interface Progress {
+   id: number;
+   isCurrent: boolean;
+   isCompleted: boolean;
+   text: string;
+}
--- frontend/src/app/modules/main/components/section-trade-progress/section-trade-progress.component.ts
+++ frontend/src/app/modules/main/components/section-trade-progress/section-trade-progress.component.ts
@@ -1,3 +1,4 @@
+import { Progress } from './../../../../core/models/progress.model';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
@@ -11,6 +12,26 @@ import * as fromApp from '../../../../store/index';
 })
 export class SectionTradeProgressComponent implements OnInit {
    progress$: Observable<number>;
+   levels: Progress[] = [
+      {
+         id: 1,
+         isCompleted: false,
+         isCurrent: true,
+         text: 'Выберите направление обмена',
+      },
+      {
+         id: 2,
+         isCompleted: false,
+         isCurrent: false,
+         text: 'Платежная информация',
+      },
+      {
+         id: 3,
+         isCompleted: false,
+         isCurrent: false,
+         text: 'Подтверждение, оплата',
+      },
+   ];
 
    constructor(private store: Store<fromApp.AppState>) {}
 
