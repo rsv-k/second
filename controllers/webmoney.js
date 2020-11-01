78e8941d7ab9259e5ef85c5da53c18261873e04e
unknown <rsv.kpast@gmail.com>
2020-11-01 15:01:05 +0200

    add xml request to webmoney X19

--- controllers/webmoney.js
+++ controllers/webmoney.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const webmoneyStatisticsHelper = require('../utils/webmoneyStatistics');
 const webmoneyHelper = require('../utils/webmoney');
 
 exports.isValidInfo = async (req, res, next) => {
@@ -10,18 +9,43 @@ exports.isValidInfo = async (req, res, next) => {
          return next(error);
       }
 
-      await webmoneyStatisticsHelper.increaseRequestsAmount();
       const reqn = webmoneyHelper.getReqn();
       const wmid = 224080027036;
       const operation = 4;
-
+      const direction = 2;
+      const purse = 'WMZ';
+      const amount = 12;
       const sign = await webmoneyHelper.getSign(
          reqn + '' + operation + '' + wmid
       );
-      console.log(reqn);
-      console.log(sign);
 
-      res.send('ok');
+      const result = await axios.post(
+         'https://apipassport.webmoney.ru/XMLCheckUser.aspx',
+         `
+         <passport.request>
+            <reqn>${reqn}</reqn>
+               <lang>en</lang>
+            <signerwmid>${wmid}</signerwmid>
+            <sign>${sign}</sign>
+            <operation>
+               <type>${operation}</type>
+               <direction>${direction}</direction>
+               <pursetype>${purse}</pursetype>
+               <amount>${amount}</amount>
+            </operation>
+            <userinfo>
+               <wmid>${wmid}</wmid>
+               <fname>Антіфоров</fname>
+               <iname>Олександр</iname>
+               <bank_name>Приват</bank_name>
+               <card_number>5375411400215278</card_number>
+            </userinfo>
+         </passport.request>
+         `,
+         { headers: { 'Content-Type': 'application/xml' } }
+      );
+
+      res.send(result.data);
    } catch (err) {
       const error = new Error('Internal server error');
       next(error);
