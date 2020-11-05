2e76f042c3d22e54333545d5a27f74b35dea45dc
unknown <rsv.kpast@gmail.com>
2020-11-05 14:08:14 +0200

    add links to pages programatically in admin-side-menu

--- frontend/src/app/modules/admin/components/admin-side-menu/admin-side-menu.component.ts
+++ frontend/src/app/modules/admin/components/admin-side-menu/admin-side-menu.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-side-menu',
-  templateUrl: './admin-side-menu.component.html',
-  styleUrls: ['./admin-side-menu.component.scss']
+   selector: 'app-admin-side-menu',
+   templateUrl: './admin-side-menu.component.html',
+   styleUrls: ['./admin-side-menu.component.scss'],
 })
 export class AdminSideMenuComponent implements OnInit {
+   links = [
+      {
+         linkTo: './orders-show',
+         icon: 'loop',
+         text: 'Заявки на обмен',
+      },
+      {
+         linkTo: './exchanges-show',
+         icon: 'shuffle',
+         text: 'Направления обмена',
+      },
+      {
+         linkTo: './currencies-show',
+         icon: 'credit_card',
+         text: 'Валюты',
+      },
+      {
+         linkTo: './validators',
+         icon: 'check',
+         text: 'Валидаторы',
+      },
+   ];
 
-  constructor() { }
+   constructor() {}
 
-  ngOnInit(): void {
-  }
-
+   ngOnInit(): void {}
 }
