80c114eb4db07a2d2bebe8e2ac67fe67c5136357
unknown <rsv.kpast@gmail.com>
2020-11-07 12:51:44 +0200

    add delete validator method in service

--- frontend/src/app/core/services/validator.service.ts
+++ frontend/src/app/core/services/validator.service.ts
@@ -6,6 +6,12 @@ import { Validator } from './../models/validator.model';
 
 const ENDPOINT_URL = '/api/validator/';
 
+interface Response {
+   msg: string;
+   validator?: any;
+   validators?: any[];
+}
+
 @Injectable({
    providedIn: 'root',
 })
@@ -13,30 +19,32 @@ export class ValidatorService {
    constructor(private http: HttpClient) {}
 
    createValidator(validator: Validator): Observable<Validator> {
-      return this.http
-         .post<{ msg: string; validator: any }>(ENDPOINT_URL, validator)
-         .pipe(
-            pluck('validator'),
-            map((v) => {
+      return this.http.post<Response>(ENDPOINT_URL, validator).pipe(
+         pluck('validator'),
+         map((v) => {
+            v.id = v._id;
+            delete v._id;
+            return v;
+         })
+      );
+   }
+
+   getValidators(): Observable<Validator[]> {
+      return this.http.get<Response>(ENDPOINT_URL).pipe(
+         pluck('validators'),
+         map((validators) =>
+            validators.map((v) => {
                v.id = v._id;
                delete v._id;
                return v;
             })
-         );
+         )
+      );
    }
 
-   getValidators(): Observable<Validator[]> {
+   deleteValidator(id: string): Observable<Validator> {
       return this.http
-         .get<{ msg: string; validators: any[] }>(ENDPOINT_URL)
-         .pipe(
-            pluck('validators'),
-            map((validators) =>
-               validators.map((v) => {
-                  v.id = v._id;
-                  delete v._id;
-                  return v;
-               })
-            )
-         );
+         .delete<Response>(ENDPOINT_URL + id)
+         .pipe(pluck('validator'));
    }
 }
