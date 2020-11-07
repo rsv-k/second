b55fd196e60ef60a8db73fd1bd8819e6f91c7fb0
unknown <rsv.kpast@gmail.com>
2020-11-07 12:41:09 +0200

    display validators table on its page

--- frontend/src/app/modules/admin/components/validators-show/validators-show.component.ts
+++ frontend/src/app/modules/admin/components/validators-show/validators-show.component.ts
@@ -1,6 +1,12 @@
+import { Validator } from './../../../../core/models/validator.model';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { ValidatorsCreateComponent } from '../validators-create/validators-create.component';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../../../../store/index';
+import * as ValidatorActions from '../../../../store/actions/validator.actions';
+import { filter, pluck } from 'rxjs/operators';
 
 @Component({
    selector: 'app-validators-show',
@@ -8,9 +14,20 @@ import { ValidatorsCreateComponent } from '../validators-create/validators-creat
    styleUrls: ['./validators-show.component.scss'],
 })
 export class ValidatorsShowComponent implements OnInit {
-   constructor(public dialog: MatDialog) {}
+   validators$: Observable<Validator[]>;
+   displayedColumns = ['name', 'pattern'];
+   constructor(
+      public dialog: MatDialog,
+      private store: Store<fromRoot.AppState>
+   ) {}
 
-   ngOnInit(): void {}
+   ngOnInit(): void {
+      this.validators$ = this.store
+         .select('validator')
+         .pipe(pluck('validators'));
+
+      this.store.dispatch(ValidatorActions.getValidatorsStart());
+   }
 
    openDialog(): void {
       const dialogRef = this.dialog.open(ValidatorsCreateComponent, {
