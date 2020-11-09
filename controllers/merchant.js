1af25be78a9fb0616fc8358cddc8d5f6a11570fb
unknown <rsv.kpast@gmail.com>
2020-11-09 19:29:03 +0200

    add merchant utils

--- controllers/merchant.js
+++ controllers/merchant.js
@@ -1,47 +1,17 @@
-const axios = require('axios');
-const webmoneyHelper = require('../utils/webmoney');
-const xml2js = require('xml2js');
+const merchantHelper = require('../utils/merchant');
 
 exports.webmoney = async (req, res, next) => {
    try {
-      const reqn = webmoneyHelper.getReqn();
-      const wmid = 224080027036;
       const operation = 4;
       const direction = 2;
       const purse = 'WMZ';
-      const amount = 12;
-      const sign = await webmoneyHelper.getSign(
-         reqn + '' + operation + '' + wmid
-      );
 
-      const result = await axios.post(
-         'https://apipassport.webmoney.ru/XMLCheckUser.aspx',
-         `
-         <passport.request>
-            <reqn>${reqn}</reqn>
-               <lang>en</lang>
-            <signerwmid>${wmid}</signerwmid>
-            <sign>${sign}</sign>
-            <operation>
-               <type>${operation}</type>
-               <direction>${direction}</direction>
-               <pursetype>${purse}</pursetype>
-               <amount>${amount}</amount>
-            </operation>
-            <userinfo>
-               <wmid>${wmid}</wmid>
-               <fname>Антіфоров</fname>
-               <iname>Олександр</iname>
-               <bank_name>Приват</bank_name>
-               <card_number>5375411400215278</card_number>
-            </userinfo>
-         </passport.request>
-         `,
-         { headers: { 'Content-Type': 'application/xml' } }
+      const response = merchantHelper.interfaceX19(
+         operation,
+         direction,
+         purse,
+         req.body.givenCurrencyAmount
       );
-
-      const json = await xml2js.parseStringPromise(result.data);
-      const response = !+json['passport.response']['retval'][0];
 
       res.status(200).json({ result: response });
    } catch (err) {
