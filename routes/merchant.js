0564c6556162c127a628dfe70e19cab1bec45b6e
unknown <rsv.kpast@gmail.com>
2020-11-09 17:19:06 +0200

    add validationResult middleware on backend

--- routes/merchant.js
+++ routes/merchant.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const merchantController = require('../controllers/merchant');
 const orderValidator = require('../validators/order');
+const validationResultMiddleware = require('../middlewares/validationResult');
 
 router.post(
-   'webmoney',
+   '/webmoney',
    orderValidator.checkBodyCorrection,
+   validationResultMiddleware,
    merchantController.webmoney
 );
 
