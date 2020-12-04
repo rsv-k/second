c3dbb24b7b45376d92a9e9cf6a1bd639bd43e639
unknown <rsv.kpast@gmail.com>
2020-12-04 17:42:03 +0200

    add merchant routes

--- backend/entryPoint.js
+++ backend/entryPoint.js
@@ -8,6 +8,7 @@ const exchangeRoutes = require('./routes/exchange');
 const orderRoutes = require('./routes/order');
 const serviceValidatorsRoutes = require('./routes/serviceValidators');
 const validatorRoutes = require('./routes/validator');
+const merchantRoutes = require('./routes/merchant');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +20,7 @@ app.use('/api/exchange', exchangeRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/merchant', serviceValidatorsRoutes);
 app.use('/api/validator', validatorRoutes);
+app.use('/api/merchant', merchantRoutes);
 
 app.use(errorHandler);
 
