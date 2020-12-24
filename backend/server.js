25bda7d314ae149e7eafdc43cc31a373a9c13011
rsv_k <rsv.kpast@gmail.com>
2020-12-24 17:15:26 +0200

    add morgan on backend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const entryPoint = require('./entryPoint');
+const morgan = require('morgan');
 
 if (!process.env.production) {
    require('dotenv').config();
 }
 
+app.use(morgan('dev'));
 app.use(entryPoint);
 
 mongoose
