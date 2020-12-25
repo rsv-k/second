1a77d507c63716ee913b963ace75fbbc55940f5e
rsv_k <rsv.kpast@gmail.com>
2020-12-25 22:17:53 +0200

    rename folder middlewares to middleware

--- backend/entryPoint.js
+++ backend/entryPoint.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const errorHandler = require('./middlewares/error');
+const errorHandler = require('./middleware/error');
 const path = require('path');
 
 const currencyRoutes = require('./routes/currencies');
--- backend/routes/currencies.js
+++ backend/routes/currencies.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const multerMiddleware = require('../middlewares/multer');
+const multerMiddleware = require('../middleware/multer');
 
 const {
    getCurrencies,
