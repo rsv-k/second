71f39c79df142279594956a45d3107f3bf9d6de2
rsv_k <rsv.kpast@gmail.com>
2020-12-26 02:01:16 +0200

    remove entryPoint file

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,24 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const colors = require('colors');
+const path = require('path');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 
 const PORT = process.env.PORT || 3000;
-const entryPoint = require('./entryPoint');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Routes
+const currencyRoutes = require('./routes/currencies');
+const exchangeRoutes = require('./routes/exchanges');
+const orderRoutes = require('./routes/orders');
+const serviceValidatorsRoutes = require('./routes/serviceValidators');
+const validatorRoutes = require('./routes/validators');
+const merchantRoutes = require('./routes/merchants');
 
 // Dev logger
 if (process.env.NODE_ENV === 'development') {
@@ -22,7 +30,14 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(entryPoint);
+app.use('/images', express.static(path.join('images')));
+
+app.use('/api/v1/currencies', currencyRoutes);
+app.use('/api/v1/exchanges', exchangeRoutes);
+app.use('/api/v1/orders', orderRoutes);
+app.use('/api/v1/serviceValidators', serviceValidatorsRoutes);
+app.use('/api/v1/validators', validatorRoutes);
+app.use('/api/v1/merchants', merchantRoutes);
 
 app.use(errorHandler);
 
