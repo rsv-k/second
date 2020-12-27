7bb67d9060d14ec079ef7e52c557af49811c82bb
rsv_k <rsv.kpast@gmail.com>
2020-12-27 11:32:46 +0200

    add fileFilter function in multer middleware

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,21 +1,29 @@
-const multer = require("multer");
+const multer = require('multer');
 
 const MIME_TYPES = {
-   "image/jpeg": "jpeg",
-   "image/png": "png",
-   "image/jpg": "jpg",
+   'image/jpeg': 'jpeg',
+   'image/png': 'png',
+   'image/jpg': 'jpg',
 };
 
 const storage = multer.diskStorage({
    destination: (req, file, cb) => {
-      cb(null, "./images");
+      cb(null, './images');
    },
    filename: (req, file, cb) => {
       cb(
          null,
-         file.fieldname + "-" + Date.now() + "." + MIME_TYPES[file.mimetype]
+         file.fieldname + '-' + Date.now() + '.' + MIME_TYPES[file.mimetype]
       );
    },
 });
 
-module.exports = multer({ storage }).single("icon");
+const fileFilter = (req, file, cb) => {
+   if (MIME_TYPES[file.mimetype]) {
+      cb(null, true);
+   } else {
+      cb(null, false);
+   }
+};
+
+module.exports = multer({ storage, fileFilter }).single('icon');
