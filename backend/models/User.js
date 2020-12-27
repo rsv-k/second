63a864d05de026c90fa2c1fcd410952af27b18b3
rsv_k <rsv.kpast@gmail.com>
2020-12-27 21:57:07 +0200

    add method on user document to get signed jwt

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
    email: {
@@ -32,5 +33,12 @@ UserSchema.pre('save', async function (next) {
    const salt = await bcrypt.genSalt(10);
    this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Generate JWT
+UserSchema.methods.getSignedJwtToken = function (next) {
+   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE,
+   });
+};
 
 module.exports = mongoose.model('User', UserSchema);
